60dde6efd63dae93bbc9b00c754cf6079a984a55
GuanYangCLU <guanyang@callutheran.edu>
2019-07-11 18:13:46 -0700

    0711_03

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,19 @@ import { addOne, subOne } from '../../redux/actions/counter';
 import { timerStartStop, timerReset } from '../../redux/actions/timer';
 // combineReducer后，state为整体reducer的obj，里面含有{counterRdc, TimerRdc,...}
 // 需要向下取具体的reducer再取key，一般就整体将一个reducer里的state拿来，用什么props点什么
-function App(props) {
+
+// 也可以直接把import过来的function放到props中省去mapDispatchToProps
+// 不过最后connect的时候要在mapStateToProps后面加引入的函数作为props
+// 有选择的插入props⬇
+function App({ addOne, subOne, timerStartStop, timerReset, output, timer }) {
   return (
     <div className='App'>
-      <div className='num'>Count: {props.output.num}</div>
-      <div className='time'>Time: {props.timer.time}</div>
-      <button onClick={() => props.timerStartStop(props.timer.on)}>
-        {props.timer.text}
-      </button>
-      <button onClick={props.addOne}>+</button>
-      <button onClick={props.subOne}>-</button>
-      <button onClick={props.timerReset}>Reset</button>
+      <div className='num'>Count: {output.num}</div>
+      <div className='time'>Time: {timer.time}</div>
+      <button onClick={() => timerStartStop(timer.on)}>{timer.text}</button>
+      <button onClick={addOne}>+</button>
+      <button onClick={subOne}>-</button>
+      <button onClick={timerReset}>Reset</button>
     </div>
   );
 }
